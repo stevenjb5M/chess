42a042f57aa04776076106ad5a5377938d4d4dd4
stevenjb5M <sb2568@byu.edu>
2025-05-21 16:37:59 -0600

    final commit for phase 3!

--- server/src/main/java/service/LoginResult.java
+++ server/src/main/java/service/LoginResult.java
@@ -1,27 +1,8 @@
 package service;
 
-public class LoginResult {
-    private String username;
-    private String authToken;
+public class LoginResult extends RegisterResult {
 
     public LoginResult(String username, String authToken) {
-        this.username = username;
-        this.authToken = authToken;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getAuthToken() {
-        return authToken;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
+        super(username, authToken);
     }
 }
