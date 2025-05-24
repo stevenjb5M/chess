c842463e7c5bde0c8d57a613121be83e989f20cd
stevenjb5M <sb2568@byu.edu>
2025-05-24 16:00:09 -0600

    stubbed out SQL USER DAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,16 +1,39 @@
 package dataaccess;
 
 import model.UserData;
-
+import javax.xml.crypto.Data;
 import java.util.HashMap;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLUserDAO implements UserDAO {
     final private HashMap<String, UserData> users = new HashMap<>();
 
+    public SQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
     @Override
     public UserData addUser(UserData user) throws DataAccessException {
-        return null;
+        UserData existingUser = users.get(user.username());
+
+        if (existingUser == null) {
+            var statement = "INSERT INTO user (username, json) VALUES (?, ?, ?)";
+            var json = new Gson().toJson(user);
+            var id = executeUpdate(statement, user.username(), json);
+
+            return user;
+        } else {
+            throw new UsernameTakenException("Error: already taken");
+        }
+
+        //not sure if we need this
+        //return new UserData(id, pet.name(), pet.type());
     }
 
     @Override
@@ -22,4 +45,108 @@ public class SQLUserDAO implements UserDAO {
     public void clearUsers() throws DataAccessException {
 
     }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("");
+        }
+    }
+
+
+//    public Pet getPet(int id) throws ResponseException {
+//        try (var conn = DatabaseManager.getConnection()) {
+//            var statement = "SELECT id, json FROM pet WHERE id=?";
+//            try (var ps = conn.prepareStatement(statement)) {
+//                ps.setInt(1, id);
+//                try (var rs = ps.executeQuery()) {
+//                    if (rs.next()) {
+//                        return readPet(rs);
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+//        }
+//        return null;
+//    }
+//
+//    public Collection<Pet> listPets() throws ResponseException {
+//        var result = new ArrayList<Pet>();
+//        try (var conn = DatabaseManager.getConnection()) {
+//            var statement = "SELECT id, json FROM pet";
+//            try (var ps = conn.prepareStatement(statement)) {
+//                try (var rs = ps.executeQuery()) {
+//                    while (rs.next()) {
+//                        result.add(readPet(rs));
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+//        }
+//        return result;
+//    }
+//
+//    public void deletePet(Integer id) throws ResponseException {
+//        var statement = "DELETE FROM pet WHERE id=?";
+//        executeUpdate(statement, id);
+//    }
+//
+//    public void deleteAllPets() throws ResponseException {
+//        var statement = "TRUNCATE pet";
+//        executeUpdate(statement);
+//    }
+//
+//    private Pet readPet(ResultSet rs) throws SQLException {
+//        var id = rs.getInt("id");
+//        var json = rs.getString("json");
+//        var pet = new Gson().fromJson(json, Pet.class);
+//        return pet.setId(id);
+//    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof UserData p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to update database: %s, %s");
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
 }
