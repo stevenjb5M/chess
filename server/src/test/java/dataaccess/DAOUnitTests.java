efe28a1df4eea3a1a12ce41ec85f649afcd0e62b
stevenjb5M <sb2568@byu.edu>
2025-05-28 19:20:52 -0600

    unit test for add user

--- server/src/test/java/dataaccess/DAOUnitTests.java
+++ server/src/test/java/dataaccess/DAOUnitTests.java
@@ -2,7 +2,9 @@ package dataaccess;
 
 import chess.ChessGame;
 import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
 import passoff.model.TestCreateRequest;
 import passoff.model.TestUser;
 import service.*;
@@ -17,16 +19,19 @@ public class DAOUnitTests {
     private static UserService userService;
     private static AuthService authService;
     private static GameService gameService;
+    private static UserDAO userDAO;
+    private static AuthDAO authDAO;
+    private static GameDAO gameDAO;
 
 
     @BeforeAll
     public static void init() {
-        SQLUserDAO userDAO = new SQLUserDAO();
-        SQLAuthDAO authDAQ = new SQLAuthDAO();
-        SQLGameDAO gameDAQ = new SQLGameDAO();
-        authService = new AuthService(authDAQ);
+        userDAO = new SQLUserDAO();
+        authDAO = new SQLAuthDAO();
+        gameDAO = new SQLGameDAO();
+        authService = new AuthService(authDAO);
         userService = new UserService(userDAO, authService);
-        gameService = new GameService(gameDAQ);
+        gameService = new GameService(gameDAO);
     }
 
     @BeforeEach
@@ -38,234 +43,35 @@ public class DAOUnitTests {
 
     @Test
     @Order(1)
-    @DisplayName("Register Positive")
-    public void registerPositive() throws DataAccessException {
+    @DisplayName("addUser Positive")
+    public void addUserPositive() throws DataAccessException {
         RegisterRequest request = new RegisterRequest("steven3", "test", "steven@gmail.com");
 
-        userService.register(request);
+        String hashedPassword = BCrypt.hashpw("password", BCrypt.gensalt());
 
-        Assertions.assertNotNull(userService.getUser("steven3"));
+        UserData userData = new UserData("steven", hashedPassword, "steven@gmail.com");
+
+        userDAO.addUser(userData);
+
+
+        Assertions.assertNotNull(userDAO.getUser("steven"));
 
     }
 
     @Test
     @Order(1)
-    @DisplayName("Register Negative")
-    public void registerNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("testuser2", null, "steven@gmail.com");
+    @DisplayName("addUser Negative")
+    public void addUserNegative() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("steven3", "test", "steven@gmail.com");
 
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            userService.register(request);
+        String hashedPassword = BCrypt.hashpw("password", BCrypt.gensalt());
+
+        UserData userData = new UserData(null, hashedPassword, "steven@gmail.com");
+
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDAO.addUser(userData);
         });
 
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Login Positive")
-    public void loginPositive() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("steven9", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        LoginRequest requestLogin = new LoginRequest("steven9", "test");
-
-        LoginResult result =  userService.login(requestLogin);
-
-        Assertions.assertTrue(authService.checkIfAuthExisits(result.getAuthToken()));
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Login Negative")
-    public void loginNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven12", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-
-        LoginRequest requestLogin = new LoginRequest("Steven12", "test123");
-
-        Assertions.assertThrows(UnauthorizedException.class, () -> {
-            userService.login(requestLogin);
-        });
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Logout Positive")
-    public void logoutPositive() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven11", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        LoginRequest requestLogin = new LoginRequest("Steven11", "test");
-
-        LoginResult loginResult = userService.login(requestLogin);
-
-        LogoutRequest logoutRequest = new LogoutRequest(loginResult.getAuthToken());
-
-        userService.logout(logoutRequest.getAuthToken());
-
-        Assertions.assertFalse(authService.checkIfAuthExisits(logoutRequest.getAuthToken()));
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Logout Negative")
-    public void logoutNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven13", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        LoginRequest requestLogin = new LoginRequest("Steven13", "test");
-
-        LoginResult loginResult = userService.login(requestLogin);
-
-        LogoutRequest logoutRequest = new LogoutRequest("");
-
-
-        Assertions.assertThrows(UnauthorizedException.class, () -> {
-            userService.logout(logoutRequest.getAuthToken());
-        });
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Create Game Positive")
-    public void createGamePositive() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest("test");
-
-        CreateGameResult result = gameService.createGame(createGameRequest);
-
-        Assertions.assertNotNull(result.getGameID());
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Create Game Negative")
-    public void createGameNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest(null);
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            CreateGameResult result = gameService.createGame(createGameRequest);
-        });
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Join Game Positive")
-    public void joinGamePositive() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest("test");
-
-        CreateGameResult result = gameService.createGame(createGameRequest);
-
-        JoinGameRequest joinRequest = new JoinGameRequest(ChessGame.TeamColor.BLACK, result.getGameID());
-
-        gameService.joinGame(joinRequest, "Steven");
-
-        String blackUsername = gameService.getBlackPlayerUsername(result.getGameID());
-
-        Assertions.assertNotNull(blackUsername);
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Join Game Negative")
-    public void joinGameNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest("test");
-
-        CreateGameResult result = gameService.createGame(createGameRequest);
-
-        JoinGameRequest joinRequest = new JoinGameRequest(ChessGame.TeamColor.BLACK, result.getGameID());
-
-        gameService.joinGame(joinRequest, "Steven");
-
-        String whiteUsername = gameService.getWhitePlayerUsername(result.getGameID());
-
-        Assertions.assertNull(whiteUsername);
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("List Games Positive")
-    public void listGamesPositive() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest("test");
-
-        CreateGameResult result = gameService.createGame(createGameRequest);
-
-        Collection<GameData> games = gameService.listGames();
-
-        Assertions.assertNotNull(games);
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("List Games Negative")
-    public void listGamesNegative() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        Collection<GameData> games = gameService.listGames();
-
-        Assertions.assertNotNull(games);
-
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("Clear Database")
-    public void clearDatabase() throws DataAccessException {
-        RegisterRequest request = new RegisterRequest("Steven", "test", "steven@gmail.com");
-
-        userService.register(request);
-
-        CreateGameRequest createGameRequest = new CreateGameRequest("test");
-
-        CreateGameResult result = gameService.createGame(createGameRequest);
-
-        JoinGameRequest joinRequest = new JoinGameRequest(ChessGame.TeamColor.BLACK, result.getGameID());
-
-        gameService.joinGame(joinRequest, "Steven");
-
-        String whiteUsername = gameService.getWhitePlayerUsername(result.getGameID());
-
-        userService.clearUsers();
-        gameService.clearGames();
-        authService.clearAuths();
-
-        Collection<GameData> games = gameService.listGames();
-
-        Assertions.assertTrue(games.isEmpty());
     }
 
 }
