019475d789fd920bc08852b108de7e7470e32a78
stevenjb5M <sb2568@byu.edu>
2025-06-11 16:56:07 -0600

    make move bad authtoken

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -37,6 +37,11 @@ public class WebSocketHandler {
                 }
             }
             case MAKE_MOVE -> {
+                try {
+                    makeMove(action.getGameID(), action.getAuthToken(), session);
+                } catch (DataAccessException e) {
+                    throw new RuntimeException(e);
+                }
             }
             case LEAVE -> {
             }
@@ -46,6 +51,49 @@ public class WebSocketHandler {
     }
 
     private void connect(int gameID, String token, Session session) throws IOException, DataAccessException {
+
+        AuthDAO authDAO = new SQLAuthDAO();
+        AuthService authService = new AuthService(authDAO);
+
+        if (!authService.checkIfAuthExisits(token)) {
+            var notificationError = ServerMessage.error("Error, invalid auth token");
+            var errorMessage = new Gson().toJson(notificationError);
+            session.getRemote().sendString(errorMessage);
+            throw new IOException("Error: Auth token was wrong");
+        }
+
+        String playername = authService.getUserByAuth(token);
+
+        GameDAO gameDAO = new SQLGameDAO();
+        GameService gameService = new GameService(gameDAO);
+        Collection<GameData> games = gameService.listGames();
+
+        connections.add(playername, session);
+
+
+
+        boolean found = false;
+        for (GameData gameData : games) {
+            if (gameData.gameID() == gameID) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            var notificationError = ServerMessage.error("Error, invalid game ID");
+            connections.broadcastLoad(playername, notificationError);
+            throw new IOException("Error: Game ID was wrong");
+        }
+
+        var message = String.format("%s has joined the game", playername);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME, gameID);
+        var notification1 = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        connections.broadcastLoad(playername, notification);
+        connections.broadcastInGame(playername, notification1);
+    }
+
+    private void makeMove(int gameID, String token, Session session) throws IOException, DataAccessException {
 
         AuthDAO authDAO = new SQLAuthDAO();
         AuthService authService = new AuthService(authDAO);
